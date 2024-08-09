97bbd332ff29141d5539ba6eb0c0f0b7ce3ba513
“Aleksanteri” <aleksanteri.heliovaara@gmail.com>
2024-08-09 16:25:38 +0300

    new deploy

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -5,22 +5,23 @@ import app from "../../firebase";
 import LoginView from "./LoginView";
 
 class LoginContainer extends Component {
-  handleSignIn = async event => {
-    event.preventDefault();
-    const { email, password } = event.target.elements;
-    try {
-      const user = await app
-        .auth()
-        .signInWithEmailAndPassword(email.value, password.value);
-      this.props.history.push("/home");
-    } catch (error) {
-      alert(error);
-    }
-  };
 
-  render() {
-    return <LoginView onSubmit={this.handleSignIn} />;
-  }
+	handleSignIn = async event => {
+		event.preventDefault();
+		const { email, password } = event.target.elements;
+		try {
+			const user = await app
+				.auth()
+				.signInWithEmailAndPassword(email.value, password.value);
+			this.props.history.push("/home");
+		} catch (error) {
+			alert(error);
+		}
+	};
+
+	render() {
+		return <LoginView onSubmit={this.handleSignIn} />;
+	}
 }
 
 export default LoginContainer;
